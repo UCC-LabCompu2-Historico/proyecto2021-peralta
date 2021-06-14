c9cfe1c77b48215eeb628c357faede975c26ee59
JulietaP2021 <2012028@ucc.edu.ar>
2021-06-14 04:42:06 -0300

    Se agregó dibujo en canvas

--- MisFunciones.js
+++ MisFunciones.js
@@ -9,13 +9,13 @@ function calcularVoltaje(){
     N1 = document.getElementById("n1").value;
     N2 = document.getElementById("n2").value;
 
-    if((V1 == "") || (N1 == "") || (N2 == "")){
+    if(V1 == "" || N1 == "" || N2 == ""){
         alert("Faltan datos");
-    }else if((N1 % 1 != 0) || (N2 % 1 != 0)){
+    }else if(N1 % 1 != 0 || N2 % 1 != 0){
         alert("Ingrese un número entero para N1 o N2");
         document.getElementById("n1").value = "";
         document.getElementById("n2").value = "";
-    }else if((V1 <= 0) || (N1 <= 0) || (N2 <= 0)){
+    }else if(V1 <= 0 || N1 <= 0 || N2 <= 0){
         alert("Los valores deben ser positivos");
         document.getElementById("v1").value = "";
         document.getElementById("n1").value = "";
@@ -35,18 +35,96 @@ function calcularIntensidad(){
     N1 = document.getElementById("n1").value;
     N2 = document.getElementById("n2").value;
 
-    if((I1 == "") || (N1 == "") || (N2 == "")){
+    if(I1 == "" || N1 == "" || N2 == ""){
         alert("Faltan datos");
-    }else if((N1 % 1 != 0) || (N2 % 1 != 0)){
+    }else if(N1 % 1 != 0 || N2 % 1 != 0){
         alert("Ingrese un número entero para N1 o N2");
         document.getElementById("n1").value = "";
         document.getElementById("n2").value = "";
-    }else if((I1 <= 0) || (N1 <= 0) || (N2 <= 0)){
+    }else if(I1 <= 0 || N1 <= 0 || N2 <= 0){
         alert("Los valores deben ser positivos");
         document.getElementById("i1").value = "";
         document.getElementById("n1").value = "";
         document.getElementById("n2").value = "";
     }else{
         document.getElementById("i2").innerHTML = Math.round(((I1*N1)/N2)*100)/100 + " amperios (A)";
+    }
+}
+
+/**
+ * Dibuja la placa del transformador eléctrico y el número de vueltas de cada bobina
+ * @method dibujarTransformador
+ * @param {number} valorN1 - El número de vueltas de la bobina primaria ingresado por el usuario
+ * @param {number} valorN2 - El número de vueltas de la bobina secundaria ingresado por el usuario
+ */
+function dibujarTransformador(valorN1, valorN2){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    var xMax = canvas.width;
+    var yMax = canvas.height;
+
+    //Placa del transformador
+    ctx.beginPath();
+    ctx.strokeStyle = "#000000";
+    ctx.strokeRect(120, 50, xMax-240, yMax-100);
+    ctx.strokeRect(170, 90, xMax-340, yMax-180);
+    ctx.stroke();
+    ctx.closePath();
+
+    //Número de vueltas de la bobina primaria
+    var i = 0;
+    var j = 5;
+    ctx.beginPath();
+    ctx.strokeStyle = "#FFB6C1";
+    ctx.moveTo(50, 99);
+    ctx.lineTo(170, 99);
+    while(i < valorN1-2){ //No se cuentan primera y última vuelta
+        ctx.moveTo(120, 99+j);
+        ctx.lineTo(170, 99+j);
+        j+=5;
+        i++;
+    }
+    ctx.moveTo(50, 99+j);
+    ctx.lineTo(170, 99+j);
+    ctx.stroke();
+    ctx.closePath();
+
+    //Número de vueltas de la bobina secundaria
+    i = 0;
+    j = 5;
+    ctx.beginPath();
+    ctx.strokeStyle = "#FFB6C1";
+    ctx.moveTo(xMax-50, 99);
+    ctx.lineTo(xMax-170, 99);
+    while(i < valorN2-2){ //No se cuentan primera y última vuelta
+        ctx.moveTo(xMax-120, 99+j);
+        ctx.lineTo(xMax-170, 99+j);
+        j+=5;
+        i++;
+    }
+    ctx.moveTo(xMax-50, 99+j);
+    ctx.lineTo(xMax-170, 99+j);
+    ctx.stroke();
+    ctx.closePath();
+
+    if(valorN1 == "" || valorN2 == ""){
+        alert("Faltan datos");
+        canvas.width = canvas.width;
+    }else if(valorN1 < 3 || valorN1 > 27){
+        alert("Para simular el valor permitido para N1 está entre 3 y 27");
+        document.getElementById("n1").value = "";
+        canvas.width = canvas.width;
+    }else if(valorN2 < 3 || valorN2 > 27 ){
+        alert("Para simular el valor permitido para N2 está entre 3 y 27");
+        document.getElementById("n2").value = "";
+        canvas.width = canvas.width;
+    }else if(valorN1 % 1 != 0 || valorN2 % 1 != 0){
+        alert("Ingrese un número entero para N1 o N2");
+        document.getElementById("n1").value = "";
+        document.getElementById("n2").value = "";
+        canvas.width = canvas.width;
     }
 }
